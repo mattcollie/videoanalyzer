553510e5b08d41cdf52b202114c643a9cbe8c3ba
matt <mattcollie@live.com>
2017-07-12 07:28:12 +1200

    quick save of a possible solution
    quick commit on a possible solution link to playing youtube videos without   to playing youtube videos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+// possible solution
+// https://stackoverflow.com/questions/17546633/enabling-youtube-playback-with-video-js-wrapper
+
 function loaded() {
     console.log('DEBUG:: loaded()');
     var analyzer = new GameAnalyzer();

7c556b44033f6c0b5a9bb4bcc55ba9fd1b13bc86
Matthew Collecutt <10001876@BCS.NET.NZ>
2017-07-12 09:07:27 +1200

    adding another option to a youtube player solution

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // possible solution
 // https://stackoverflow.com/questions/17546633/enabling-youtube-playback-with-video-js-wrapper
+// https://tutorialzine.com/2015/08/how-to-control-youtubes-video-player-with-javascript
 
 function loaded() {
     console.log('DEBUG:: loaded()');

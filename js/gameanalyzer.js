61b1c2bfe7decc599ce90b2170a64db9a36c755a
matt <mattcollie@live.com>
2017-07-12 07:21:17 +1200

    adding test action
    adding a test action with default action if none is supplied.

--- js/gameanalyzer.js
+++ js/gameanalyzer.js
@@ -4,7 +4,17 @@
             container: createElement('div', {}, {backgroundColor: 'black', height:'600px', width: '600px'})
         };
         
+        var testAction = createAction('test');
+        _content.container.appendChild(testAction);
+
         return _content.container;
+    }
+
+    function createAction(name, action = function defaultAction() { console.log('DEBUGG:: ' + name + ' clicked'); }) {
+        var element = createElement('div', {}, {backgroundColor: 'yellow', height:'60px', width: '60px'});
+        element.textContent = name;
+        element.addEventListener('click', action);
+        return element;
     }
 
     function createElement(elementType = 'div', attributes = {}, styles = {}) {
